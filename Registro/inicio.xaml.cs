6a4d160524e847a36aa12c42eb7686386386e757
Edison <Edison@DESKTOP-5DQB54I>
2023-04-24 13:36:49 -0500

    Add project files.

---
+++ Registro/inicio.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Registro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class inicio : ContentPage
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string dato1 = txtDato.Text;
+            string dato2 = txtDatodos.Text;
+
+            Navigation.PushAsync(new Registro(dato1, dato2));
+        }
+    }
+}
